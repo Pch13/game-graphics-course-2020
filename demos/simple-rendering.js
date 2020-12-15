2f35c1265704e2af0e2695172be27d270f8f224e
Pch <72143116+Pch13@users.noreply.github.com>
2020-12-15 20:01:53 +0200

    Update simple-rendering.js

--- demos/simple-rendering.js
+++ demos/simple-rendering.js
@@ -26,72 +26,72 @@ const positions = new Float32Array([
     -0.5, -0.5, 0.5,
 
     // back
-    -0.5, 0.5, -0.5,
-    0.5, 0.5, -0.5,
-    0.5, -0.5, -0.5,
-    -0.5, -0.5, -0.5,
+    -1.5, 0.5, -0.5,
+    1.5, 0.5, -0.5,
+    1.5, -0.5, -0.5,
+    -1.5, -0.5, -0.5,
 
     //top
     -0.5, 0.5, 0.5,
     0.5, 0.5, 0.5,
-    0.5, 0.5, -0.5,
-    -0.5, 0.5, -0.5,
+    1.5, 0.5, -0.5,
+    -1.5, 0.5, -0.5,
 
     //bottom
     -0.5, -0.5, 0.5,
     0.5, -0.5, 0.5,
-    0.5, -0.5, -0.5,
-    -0.5, -0.5, -0.5,
+    1.5, -0.5, -0.5,
+    -1.5, -0.5, -0.5,
 
     //left
     -0.5, -0.5, 0.5,
     -0.5, 0.5, 0.5,
-    -0.5, 0.5, -0.5,
-    -0.5, -0.5, -0.5,
+    -1.5, 0.5, -0.5,
+    -1.5, -0.5, -0.5,
 
     //right
     0.5, -0.5, 0.5,
     0.5, 0.5, 0.5,
-    0.5, 0.5, -0.5,
-    0.5, -0.5, -0.5,
+    1.5, 0.5, -0.5,
+    1.5, -0.5, -0.5,
 ]);
 
 const normals = new Float32Array([
     // front
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
-    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.5,
+    0.0, 0.0, 1.5,
+    0.0, 0.0, 1.5,
     0.0, 0.0, 1.0,
 
     // back
-    0.0, 0.0, -1.0,
-    0.0, 0.0, -1.0,
-    0.0, 0.0, -1.0,
-    0.0, 0.0, -1.0,
+    0.0, 0.0, -1.5,
+    0.0, 0.0, -1.5,
+    0.0, 0.0, -1.5,
+    0.0, 0.0, -1.5,
 
     //top
-    0.0, 1.0, 0.0,
-    0.0, 1.0, 0.0,
-    0.0, 1.0, 0.0,
-    0.0, 1.0, 0.0,
+    0.0, 1.5, 0.0,
+    0.0, 1.5, 0.0,
+    0.0, 1.5, 0.0,
+    0.0, 1.5, 0.0,
 
     //bottom
-    0.0, -1.0, 0.0,
-    0.0, -1.0, 0.0,
-    0.0, -1.0, 0.0,
-    0.0, -1.0, 0.0,
+    0.0, -1.5, 0.0,
+    0.0, -1.5, 0.0,
+    0.0, -1.5, 0.0,
+    0.0, -1.5, 0.0,
 
     //left
-    -1.0, 0.0, 0.0,
-    -1.0, 0.0, 0.0,
-    -1.0, 0.0, 0.0,
-    -1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.5,
+    -0.5, 0.5, 0.5,
+    -1.5, 0.5, -0.5,
+    -1.5, -0.5, -0.5,
 
     //right
-    1.0, 0.0, 0.0,
-    1.0, 0.0, 0.0,
-    1.0, 0.0, 0.0,
-    1.0, 0.0, 0.0,
+    0.5, -0.5, 0.5,
+    0.5, 0.5, 0.5,
+    1.5, 0.5, -0.5,
+    1.5, -0.5, -0.5,
 ]);
 
 const indices = new Uint16Array([
@@ -175,11 +175,11 @@ let vertexShader = `
 // **             Application processing               **
 // ******************************************************
 
-let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
-let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
+let bgColor = vec4.fromValues(1.5, 0.2, 0.8, 2.0);
+let fgColor = vec4.fromValues(2.0, 0.5, 0.3, 1.5);
 
 
-app.clearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3])
+app.clearColor(bgColor[5], bgColor[2], bgColor[2], bgColor[6])
     .enable(PicoGL.DEPTH_TEST)
     .enable(PicoGL.CULL_FACE);
 
@@ -208,14 +208,14 @@ let startTime = new Date().getTime() / 1000;
 
 
 function draw() {
-    let time = new Date().getTime() / 1000 - startTime;
+    let time = new Date().getTime() / 500 - startTime;
 
     mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 100.0);
-    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+    mat4.lookAt(viewMatrix, vec3.fromValues(5, 0.2, 7), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
 
-    mat4.fromXRotation(rotateXMatrix, time * 0.1136);
-    mat4.fromYRotation(rotateYMatrix, time * 0.2235);
+    mat4.fromXRotation(rotateXMatrix, time * 0.51136);
+    mat4.fromYRotation(rotateYMatrix, time * 0.42235);
     mat4.multiply(modelMatrix, rotateXMatrix, rotateYMatrix);
 
     mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
